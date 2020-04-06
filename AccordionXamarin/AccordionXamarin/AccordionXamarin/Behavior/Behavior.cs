23c2c4e082c6f9baae612db0d6281b6be38c34a3
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-06 17:54:55 +0530

    XAMARIN-34112 - Sample added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Behavior/Behavior.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.XForms.Accordion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin.Behavior
+{
+    public class Behavior : Behavior<SfAccordion>
+    {
+        #region Fields
+
+        SfAccordion Accordion;
+        #endregion
+
+        #region Overrides
+
+        protected override void OnAttachedTo(SfAccordion bindable)
+        {
+            Accordion = bindable;
+            Accordion.Expanding += Accordion_Expanding;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(SfAccordion bindable)
+        {
+            Accordion.Expanding -= Accordion_Expanding;
+            Accordion = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Events
+
+        private void Accordion_Expanding(object sender, ExpandingAndCollapsingEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("", "AccordionItem expanded at index " + e.Index, "Ok");
+        }
+        #endregion
+    }
+}
---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
